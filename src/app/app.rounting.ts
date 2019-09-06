7bb48c679acfe7d0be861cadba945843666ca47c
JOrge salgado <jasen192021@gmail.com>
2019-09-05 20:33:21 -0500

    creando componente

--- src/app/app.rounting.ts
+++ src/app/app.rounting.ts
@@ -1,13 +1,30 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes} from "@angular/router"; 
 import { LoginComponent } from './login/login.component';
+import { ThemeComponent } from './theme/theme.component';
+
 
 
 export const routes: Routes=[{
 path:'login',
 component:LoginComponent,
 
-}]
+},
+
+
+{
+    path:'pages',
+    component:ThemeComponent,
+    children: [
+        {
+path:'',
+loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
+        }
+    ]
+
+}
+
+]
 
 @NgModule ({
     imports:[RouterModule.forRoot(routes,{useHash:true})
